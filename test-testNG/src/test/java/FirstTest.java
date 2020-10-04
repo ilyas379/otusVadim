f61ab7c8838f814d8d2a80750d7bf6f1bc43e742
ilya gerasimchuk <ilyas379@mail.ru>
2020-10-05 00:37:50 +0400

    Initial commit

---
+++ test-testNG/src/test/java/FirstTest.java
@@ -0,0 +1,18 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+    private static final Logger logger = LogManager.getLogger(FirstTest.class);
+    public static void main(String[] args) {
+
+    }
+    @Test
+    public void test1(){
+        logger.info("INFO1");
+        logger.debug("DEBUG1");
+        logger.error("ERROR1");
+        logger.fatal("FATAL1");
+        logger.warn("WARN1");
+    }
+}
